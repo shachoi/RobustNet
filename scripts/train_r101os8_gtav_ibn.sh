2e49a98dad25e4d79fd1d753b5781b33d732e094
shachoi <belle79@gmail.com>
2021-09-14 15:57:22 +0900

    updated script for resnet-101-os8 for GTAV dataset

---
+++ scripts/train_r101os8_gtav_ibn.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+    # Example on Cityscapes
+     python -m torch.distributed.launch --nproc_per_node=4 train.py \
+        --dataset gtav \
+        --covstat_val_dataset gtav \
+        --val_dataset bdd100k cityscapes synthia mapillary \
+        --arch network.deepv3.DeepR101V3PlusD_OS8 \
+        --city_mode 'train' \
+        --lr_schedule poly \
+        --lr 0.01 \
+        --poly_exp 0.9 \
+        --max_cu_epoch 10000 \
+        --class_uniform_pct 0.5 \
+        --class_uniform_tile 1024 \
+        --crop_size 768 \
+        --scale_min 0.5 \
+        --scale_max 2.0 \
+        --rrotate 0 \
+        --max_iter 40000 \
+        --bs_mult 4 \
+        --gblur \
+        --color_aug 0.5 \
+        --wt_reg_weight 0.0 \
+        --relax_denom 0.0 \
+        --cov_stat_epoch 0 \
+        --wt_layer 4 4 0 4 4 0 0 \
+        --date 0101 \
+        --exp r101os8_gtav_ibn \
+        --ckpt ./logs/ \
+        --tb_path ./logs/
---
+++ scripts/train_r101os8_gtav_isw.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+    # Example on Cityscapes
+     python -m torch.distributed.launch --nproc_per_node=4 train.py \
+        --dataset gtav \
+        --covstat_val_dataset gtav \
+        --val_dataset bdd100k cityscapes synthia mapillary \
+        --arch network.deepv3.DeepR101V3PlusD_OS8 \
+        --city_mode 'train' \
+        --lr_schedule poly \
+        --lr 0.01 \
+        --poly_exp 0.9 \
+        --max_cu_epoch 10000 \
+        --class_uniform_pct 0.5 \
+        --class_uniform_tile 1024 \
+        --crop_size 768 \
+        --scale_min 0.5 \
+        --scale_max 2.0 \
+        --rrotate 0 \
+        --max_iter 40000 \
+        --bs_mult 4 \
+        --gblur \
+        --color_aug 0.5 \
+        --wt_reg_weight 0.6 \
+        --relax_denom 0.0 \
+        --clusters 3 \
+        --cov_stat_epoch 5 \
+        --trials 10 \
+        --wt_layer 2 2 0 2 2 0 0 \
+        --date 0101 \
+        --exp r101os8_gtav_isw \
+        --ckpt ./logs/ \
+        --tb_path ./logs/
